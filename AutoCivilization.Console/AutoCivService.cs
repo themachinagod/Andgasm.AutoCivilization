bec874128d5b3387df0b212bb0eb1d2b091a0da6
Danny Brown <machinagod@googlemail.com>
2021-01-14 09:55:40 +0000

    initial console service as IHostedService

---
+++ AutoCivilization.Console/AutoCivService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutoCivilization.Console
+{
+    public class AutoCivService : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await Task.CompletedTask;
+        }
+
+        public override async Task StartAsync(CancellationToken stoppingToken)
+        {
+            await base.StartAsync(stoppingToken);
+
+
+        }
+
+        public override async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await base.StopAsync(cancellationToken);
+        }
+    }
+}
--- AutoCivilization.Console/Program.cs
+++ AutoCivilization.Console/Program.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 
 namespace AutoCivilization.Console
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<AutoCivService>();
+                });
     }
 }
