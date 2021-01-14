383f4d96b8eb5769bc1684da0da1d11c9a829b82
Danny Brown <machinagod@googlemail.com>
2021-01-14 09:36:37 +0000

    Add project files.

---
+++ AutoCivilization.Abstractions/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AutoCivilization.Abstractions
+{
+    public class Class1
+    {
+    }
+}
---
+++ AutoCivilization.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoCivilization.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
